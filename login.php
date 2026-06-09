f1c31074e78615664433a707a7b68e95692fc1db
rezhaa-go <rezhaap2007@gmail.com>
2026-06-09 21:12:22 +0700

    Delete login.php

--- login.php
+++
@@ -1,9 +0,0 @@
-<form action="login_process.php" methode ="post">
-    <label> Username:
-        <input type="text" name="username"><br>
-    </label>
-    <label> Password:
-        <input type="password" name="password"><br>
-    </label>
-    <input type="submit" value="Login">
-</form>
