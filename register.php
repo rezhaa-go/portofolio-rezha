f722993986056e16f121f40d5682054068a00b39
rezhaa-go <rezhaap2007@gmail.com>
2026-06-09 21:13:12 +0700

    Delete register.php

--- register.php
+++
@@ -1,6 +0,0 @@
-<h1>Register</h1>
-<form action="register_process.php" method="post">
-    <input type="text" name="username">
-    <input type="password" name="password">
-    <input type="submit" value="Register">
-</form>
